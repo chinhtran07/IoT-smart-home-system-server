3cd95ddedf7eb90b5b0dae471fe002952db0ae53
chinhtran07 <chinh.t1807@gmail.com>
2024-08-21 18:10:39 +0700

    delete

--- src/routes/users.routes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const userController = require('../controllers/users.controller');
-const authMiddleware = require('../middlewares/auth.middleware');
-
-router.get('/current-user', authMiddleware.authenticateToken, userController.getCurrentUser);
-
-module.exports = router;
